127537dbd409b33bd2e07939c8e97fe1dd2ef4a5
EDcodezs <enioluwadada10@gmail.com>
2026-08-13 20:57:41 +0100

    Add files via upload

---
+++ creating_object.js
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+<title>Change Colours</title>
+</head>
+
+<body>
+<h1>Colour Changer</h1> 
+<p>This is some text on my page.</p>
+<h2>This is another heading.</h2> 
+<button onclick="changeColour()">Change Colour</button>
+<script> function changeColour() { document.body.style.color = "blue"; } </script>
+
+</body>
+</html>
